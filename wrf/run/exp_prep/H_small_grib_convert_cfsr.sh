1ae5581161da2a3816f14ad6a5fb8538395fb330
gustfrontar <gustfrontar@gmail.com>
2017-02-02 14:49:23 +0900

    small update

--- wrf/run/exp_prep/H_small_grib_convert_cfsr.sh
+++ wrf/run/exp_prep/H_small_grib_convert_cfsr.sh
@@ -7,19 +7,24 @@ ETIME=20091231180000
 INT=21600
 PARALLEL=8
 
-REGNAME="ARGENTINA"
-LATRANGE="-50:-20"
-LONRANGE="270:310"
+#Select a region or create a new region.
+REGNAME="EUROPE"
+LATRANGE="30:80"
+LONRANGE="-30:40"
+
+#REGNAME="ARGENTINA"
+#LATRANGE="-50:-20"
+#LONRANGE="270:310"
 
 #REGNAME="JAPAN"
 #LATRANGE="25:50"
 #LONRANGE="120:150"
 
-
 source ../util.sh
 
-DESTDIR=$HOME/share/DATA//CFSR/$REGNAME
+ORIGINALDIR=$HOME/share/DATA/GRIB/CFSR/HIRES/GLOBAL/
 
+DESTDIR=$HOME/share/DATA/GRIB/CFSR/HIRES/$REGNAME
 
 
 CTIME=$ITIME
@@ -29,19 +34,18 @@ do
  cparallel=1
  while [ $cparallel -le $PARALLEL -a $CTIME -le $ETIME  ]
  do 
-  echo "Voy a convertir el CFSR correspondiente a la fecha: $CTIME"
-  FECHA=`echo $CTIME | cut -c1-8`
-  ANIO=`echo $CTIME | cut -c1-4`
-  MES=`echo $CTIME | cut -c5-6`
-  DIA=`echo $CTIME | cut -c7-8`
-  HORA=`echo $CTIME | cut -c9-10`
+  echo "Reducing the file corresponding to the following time: $CTIME"
+  YEAR=`echo $CTIME  | cut -c1-4`
+  MONTH=`echo $CTIME | cut -c5-6`
+  DAY=`echo $CTIME   | cut -c7-8`
+  HOUR=`echo $CTIME  | cut -c9-10`
 
-  wgrib2 $DESTDIR/pgbh00.gdas.${ANIO}${MES}${DIA}${HORA}.grb2 -set_grib_type jpeg -match ":(UGRD|VGRD|TMP|HGT|RH|PRES|SOILW|PRMSL|LAND|ICEC|WEASD):" -small_grib $LONRANGE $LATRANGE $DESTDIR/${ANIO}${MES}${DIA}${HORA}0000.grb  > ./tmp.log  &
+  wgrib2 $DESTDIR/pgbh00.gdas.${YEAR}${MONTH}${DAY}${HOUR}.grb2 -set_grib_type jpeg -match ":(UGRD|VGRD|TMP|HGT|RH|PRES|SOILW|TSOIL|PRMSL|LAND|ICEC|WEASD):" -small_grib $LONRANGE $LATRANGE $DESTDIR/${YEAR}${MONTH}${DAY}${HOUR}0000.grb  > ./tmp.log  &
 
 
   CTIME=`date_edit2 $CTIME $INT`
   cparallel=`expr $cparallel + 1 `
-#echo $CTIME
+
   done
   time wait
 done
