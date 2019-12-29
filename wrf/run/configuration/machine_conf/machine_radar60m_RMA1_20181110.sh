54556234906ebb7ce09afb92e62dd7ccc77c4fd2
Juan Ruiz <jruiz@hydra.cima.fcen.uba.ar>
2019-12-29 12:22:53 -0300

    New experiment

--- wrf/run/configuration/machine_conf/machine_radar60m_RMA1_20181110.sh
+++ wrf/run/configuration/machine_conf/machine_radar60m_RMA1_20181110.sh
@@ -1,7 +1,7 @@
 #MACHINE CONFIGURATION
 GROUP=""
 SYSTEM=1  # 0 - K computer , 1 - qsub cluster
-PROC_PER_NODE=48    #computer number of procs per node.
+PROC_PER_NODE=24    #computer number of procs per node.
 PROC_PER_MEMBER=4   #Number  of procs per ensemble members (torque)
 NODES_PER_MEMBER=1  #Number of nodes per ensemble member.
 PPSSERVER=tormenta  #Hostname of pps server (for perturbation generation and post processing)
@@ -9,7 +9,7 @@ MAX_RUNNING=5       #Maximum number of simultaneous processes running in PPS ser
 ELAPSE="00:10:00"   #MAXIMUM ELAPSE TIME (MODIFY ACCORDING TO THE SIZE OF THE DOMAIN AND THE RESOLUTION)
 MAX_BACKGROUND_JOBS=128
 
-TOTAL_NODES_FORECAST=6
+TOTAL_NODES_FORECAST=5
 TOTAL_NODES_LETKF=2
 
 #These options control job split (in case of big jobs)
