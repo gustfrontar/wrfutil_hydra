771ef69f0028b025e9300bf3b209cd9543e7300d
Juan Ruiz <gustfrontar@gmail.com>
2023-11-08 02:09:17 +0900

    Improve spin-up function and add the possibility to use met em files from other experiments.

--- wrfutilV3.1/lib/timeutils.sh
+++ wrfutilV3.1/lib/timeutils.sh
@@ -36,6 +36,44 @@ date_ceil () {
 	fi
 }
 
+maximum_common_divisor () {
+
+   local n1=$1
+   local n2=$2
+   local gcd=0
+   if test $n1 -gt $n2 ; then
+      local i=1
+      while test $i -le $n1 ; do
+        local a=`expr $n1 % $i`
+        local b=`expr $n2 % $i`
+        if test $a -eq 0 -a $b -eq 0 ; then
+           if test $gcd -lt $i ; then
+             gcd=$i
+           fi
+        fi
+        i=`expr $i + 1`
+      done
+   fi
+   if test $n2 -gt $n1 ; then
+      i=1
+      while test $i -le $n2 ; do
+        a=`expr $n1 % $i`
+        b=`expr $n2 % $i`
+        if test $a -eq 0 -a $b -eq 0 ; then
+           if test $gcd -lt $i ; then
+             gcd=$i
+           fi
+        fi
+        i=`expr $i + 1`
+      done
+   fi
+
+   if test $n2 -eq $n1 ; then
+     gcd=$n2
+   fi
+
+   echo $gcd
+}
 
 
 
