21da97d6dd2485452a3511fd67ed17670b3c557f
Juan Ruiz <jruiz@hydra.cima.fcen.uba.ar>
2023-11-13 00:15:14 -0300

    Add simple python ploting tool

---
+++ wrfutilV3.1/bin/python/plot_tools/main_plot_exp.py
@@ -0,0 +1,32 @@
+import numpy as np
+from netCDF4 import Dataset
+from wrf import getvar
+import glob
+import mod_plot as mp
+
+#Get env variables and call functions accordingly. 
+conf=dict()
+
+#conf['exp_type']      = os.getenv( 'PLOT_TYPE' ) #FCST,ANAL,DAFCST
+#conf['exp_ini_date']   = os.getenv( 'FECHA_INI' )
+#conf['exp_end_date']   = os.getenv( 'FECHA_FIN' )
+#conf['basedir']        = os.getenv( 'BASEDIR' )   #Base directory of the experiment.
+#conf['histdir']        = os.getenv( 'HISTDIR' )   #Directory containing the experiment data.
+#conf['plotdir']        = os.getenv( 'PLOTDIR' )   #Directory where plots will be created.
+#conf['plot_type_list'] = os.getenve( 'PLOT_TYPE_LIST' ) #TODO create a plot.conf file and add this to this file.
+
+conf['exp_type']       = 'FCST'
+conf['exp_ini_date']   = '2019-10-10 18:00:00'
+conf['exp_end_date']   = '2019-10-12 18:00:00'
+conf['basedir']        = '/home/jruiz/salidas/data_assimilation_exps/PREVENIR_LOWRES_20191010/'
+conf['histdir']        = conf['basedir'] + '/HIST/' 
+conf['plotdir']        = conf['basedir'] + '/PLOT/'
+conf['plot_type_list'] = ['MAXDBZ']
+
+#Plot types
+plot_types=dict()
+plot_types['MAXDBZ'] = {'pvar1':'mdbz','pvar1dim':2,'pvar1vlev':None,'pvar2':'W','pvar2dim':3,'pvar2vlev':np.array([5.0]),'pvar1cmap':'gist_ncar','pvar1clev':np.arange(5., 75., 5.),'pvar2clev':np.arange(5.,75.,10.)}
+
+
+  
+mp.ploter_loop( conf , plot_types ) 
---
+++ wrfutilV3.1/bin/python/plot_tools/mod_plot.py
@@ -0,0 +1,110 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+from matplotlib.colors import from_levels_and_colors
+import matplotlib.patches as mpatches
+from cartopy import crs
+from cartopy.feature import NaturalEarthFeature, COLORS
+from netCDF4 import Dataset
+from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim,
+                 cartopy_ylim, latlon_coords , vinterp , destagger )
+import glob
+
+
+def ploter_loop( conf , plot_types ) :
+
+ if conf['exp_type'] == 'FCST' :
+   base_data_dir = conf['histdir'] + '/' + conf['exp_type'] + '/' 
+
+   #Get the list of date paths
+   date_list = glob.glob( base_data_dir + '/*' )
+
+   for my_date in date_list  :
+       attribs=dict()
+       attribs['date'] = os.path.basename( my_date ) 
+       print( 'Ploting date ', attribs['date']  )
+
+       #Get the list of ensemble member paths
+       mem_list = glob.glob( my_date + '/*' )
+       for my_mem in mem_list :
+          attribs['mem'] = os.path.basename( my_mem ) 
+          print( 'Ploting member ', attribs['mem'] )
+          file_list = glob.glob( my_mem + '/wrfout*' )
+          for my_file in file_list :
+            attribs['file'] = os.path.basename( my_file ) 
+            attribs['file_path'] = my_file
+            print('Ploting file ', attribs['file'] )
+         
+            for my_type in conf['plot_type_list'] :
+                get_figure( conf , plot_types[ my_type ] , attribs )
+
+
+def get_figure( conf , plot_type , attribs ) :
+
+   if plot_type['pvar1'] is not None :  #Lets plot variable 1 as shaded.
+
+      ncfile = Dataset(attribs['file_path'])
+      pvar1 = getvar(ncfile,plot_type['pvar1'])
+      if plot_type['pvar1vlev'] is not None :
+         if plot_type['pvar2'] == 'W' :
+            pvar2=destagger(pvar2,0)
+         pvar1 = vinterp( ncfile , field=pvar1 , vert_coord='ght_msl',interp_levels=plot_type['pvar1vlev'],extrapolate=True,log_p=True)
+
+      lat , lon = latlon_coords( pvar1 ) 
+      pvar1 = to_np( pvar1 )
+
+   if plot_type['pvar2'] is not None :  #Lets plot variable 2 as countour 
+      if ~ ncfile._isopen  :
+        ncfile = Dataset(attribs['file_path'])
+      pvar2 = getvar(ncfile,plot_type['pvar2'])
+      lat , lon = latlon_coords( pvar2 )
+      
+      if plot_type['pvar2vlev'] is not None :
+         if plot_type['pvar2'] == 'W' : 
+            pvar2=destagger(pvar2,0)
+         pvar2 = vinterp( ncfile , field=pvar2 , vert_coord='ght_msl',interp_levels=plot_type['pvar2vlev'],extrapolate=True,log_p=True) 
+      pvar2 = np.squeeze( to_np( pvar2 ) )
+
+   lon = to_np( lon ) ; lat = to_np( lat )
+   plot_date = os.path.basename( attribs['file_path'] )[11:]  
+   print( plot_date )
+   plot_title=''
+   file_name = str( plot_date ) + '.png'
+   fig = plt.figure(figsize=(8,6))
+   #ax = plt.axes(projection=cart_proj) #create axes
+   # Download and add the states and coastlines
+   #states = NaturalEarthFeature(category="cultural", scale="10m",
+   #                                      facecolor="none",name="admin_1_states_provinces_shp")
+   #ax.add_feature(states, linewidth=.5, edgecolor="black")
+   #ax.coastlines('50m', linewidth=0.8)
+   if plot_type['pvar2'] is not None :  #Lets plot variable 1 as shade
+      print( plot_type['pvar1'] + ':' , pvar1.min() , pvar1.max() )
+      plt.contourf( lon , lat , pvar1 ,  cmap=plot_type['pvar1cmap'] , levels=plot_type['pvar1clev'] , extend='both')
+      # Add a color bar
+      plt.colorbar(shrink=.98)
+      if plot_type['pvar1vlev'] is not None :
+         file_name =  str( plot_type['pvar1vlev'][0] ) + '_' + file_name
+         plot_title = '@' + str( plot_type['pvar1vlev'][0] ) + plot_title 
+      file_name = plot_type['pvar1'] + '_' + file_name
+      plot_title = plot_type['pvar1'] + ' ' + plot_title 
+   if plot_type['pvar2'] is not None :  #Lets plot variable 2 as countour
+      
+      print( plot_type['pvar2'] + ':' , pvar2.min() , pvar2.max() )
+      plt.contour( lon , lat , pvar2 , levels=plot_type['pvar2clev'] )
+      if plot_type['pvar2vlev'] is not None :
+         file_name =  str( plot_type['pvar2vlev'][0] ) + '_' + file_name
+         plot_title = '@' + str( plot_type['pvar2vlev'][0] )+ ' ' + plot_title 
+      file_name = plot_type['pvar2'] + '_' + file_name
+      plot_title = plot_type['pvar2'] + ' ' + plot_title  
+   # Set the map bounds
+   plt.xlim((np.min(lon),np.max(lon)))
+   plt.ylim((np.min(lat),np.max(lat)))
+   # Add the gridlines
+   plt.grid()
+   file_name=file_name.replace(':','_')
+   file_path=conf['plotdir'] + '/' + conf['exp_type'] + '/' + attribs['date'] + '/' + attribs['mem'] + '/' 
+   os.makedirs(file_path , exist_ok=True)
+   plt.title( plot_title )
+   plt.savefig( file_path + file_name , dpi=None, facecolor='w',edgecolor='w' )
+   plt.close()
