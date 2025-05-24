739883aadb3ec3d7813e7e4a529caba6898172fe
Juan Ruiz <gustfrontar@gmail.com>
2025-05-25 00:38:17 +0900

    Minor update

--- wrfutilV3.1/bin/python/post/main_post.sh
+++ wrfutilV3.1/bin/python/post/main_post.sh
@@ -6,7 +6,7 @@
 
 POSTTYPE="GUES"   #Postprocesing type, can be one of: ANAL/GUES/DAFCST
 
-EXPPATH=$HOME/data/data_assimilation_exps/PREVENIR_LOWRESOLUTION_DA_FUGAKU_2024031900_VR/
+EXPPATH=$HOME/data/data_assimilation_exps/PREVENIR_LOWRESOLUTION_DA_FUGAKU_2024031900/
 
 DATADIR="$EXPPATH/HIST/${POSTTYPE}/"      #Root path of WRFOUT files.
 OUTDIR="$EXPPATH/POST/${POSTTYPE}/"       #Root path for postprocessing output.
@@ -15,7 +15,7 @@ SRCDIR="$EXPPATH/bin/python/post/src/"    #Postprocessing code dir.
 NMEM=60          #Ensemble size
 MAXJOBS=30       #Maximum simultaneous jobs
 memfmt="%03g"    #Output format for ens members.
-anafreq=3600     #Analysis frequency (ANAL or GUES only)
+anafreq=300      #Analysis frequency (ANAL or GUES only)
 forinifreq=10800 #Forecast initialization frequency (DAFCST only)
 foroutfreq=1800  #Forecast output frequency (DAFCST only)
 forlen=86400     #FOrecast length (DAFCST only)
@@ -38,21 +38,27 @@ if [ $POSTTYPE == "ANAL" ] || [ $POSTTYPE == "GUES"  ] ; then
     ctime=$timestart
     while [ $(date -ud "$ctime" +%s) -le $(date -ud "$timeend" +%s) ] ;do 
       timed=$(date -ud "$ctime" +"%Y%m%d%H%M%S")
-      if [ $POSTTYPE == "ANAL" ] ; then
-        file=$DATADIR/$timed/anal$(printf %05g $((10#$mem)))
-      elif [ $POSTTYPE == "GUES" ] ; then
-        file=$DATADIR/$timed/gues$(printf %05g $((10#$mem)))
-      fi 
-      echo "Runing posprocessing for file $file"
-      icount=$((icount+1))
-      out=$OUTDIR/$timed/
-      outdate=$(date -ud "$ctime" +"%Y%m%d%_H%M%S")
-      mkdir -p $out
-      python $SRCDIR/post_ens_wrfout_to_netcdf.py "$file" "$out" "$mem" "$outdate" &>> $out/log_$mem &
-      exec_post "$file" "$mem" "$out" "$outdate" &
-      if [ $icount == $MAXJOBS ] ;then
-        wait
-        icount=0
+      if [ -d "$DATADIR/$timed/" ] ; then
+
+        if [ $POSTTYPE == "ANAL" ] ; then
+          file=$DATADIR/$timed/anal$(printf %05g $((10#$mem)))
+        elif [ $POSTTYPE == "GUES" ] ; then
+          file=$DATADIR/$timed/gues$(printf %05g $((10#$mem)))
+        fi 
+        echo "Runing posprocessing for file $file"
+        icount=$((icount+1))
+        out=$OUTDIR/$timed/
+        outdate=$(date -ud "$ctime" +"%Y%m%d%_H%M%S")
+        mkdir -p $out
+        python $SRCDIR/post_ens_wrfout_to_netcdf.py "$file" "$out" "$mem" "$outdate" &>> $out/log_$mem &
+        exec_post "$file" "$mem" "$out" "$outdate" &
+        if [ $icount == $MAXJOBS ] ;then
+          wait
+          icount=0
+        fi
+      else 
+        echo "Warning: Could not find analysis at $timed"
+        echo "Continue to the next time."
       fi
       ctime=$(date -ud "$ctime UTC + $anafreq seconds" +"%F %T")
     done
